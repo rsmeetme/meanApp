c3565545e0ad049cb01937466f91fb4d9de67c9b
rsmeetme <rsmeetme@gmail.com>
2021-12-20 17:50:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb?retryWrites=true&w=majority", (err)=>{
+mongoose.connect("mongodb://localhost:27017/meandb", (err)=>{
     if(!err){
         console.log('DB Connection Successfully');
     }else{
