c1380835983e2dd53d03cc87ba176e564d452c14
rajender <rsmeetme@gmail.com>
2021-12-20 17:48:21 +0530

    replace connection string

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/meandb", (err)=>{
-    if(!err){
+mongoose.connect("mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb?retryWrites=true&w=majority", (err) => {
+    if (!err) {
         console.log('DB Connection Successfully');
-    }else{
+    } else {
         console.log('Error in connection' + err);
     }
 });
