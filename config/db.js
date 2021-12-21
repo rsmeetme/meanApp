83df7a7997a36c0a35b945e7226f4f9149d48188
rajender <rsmeetme@gmail.com>
2021-12-21 15:56:55 +0530

    edited

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb", (err) => {
-    if (!err) {
-        console.log('DB Connection Successfully');
-    } else {
-        console.log('Error in connection' + err);
-    }
-});
+const mongoose = require("mongoose");
+const connection =
+    "mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb?retryWrites=true&w=majority";
+mongoose
+    .connect(connection)
+    .then(() => console.log("Database Connected Successfully"))
+    .catch((err) => console.log(err));
 
 module.exports = mongoose;
