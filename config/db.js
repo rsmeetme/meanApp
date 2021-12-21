2577ac27489896a751321786033cd7ca0128f7a4
rajender <rsmeetme@gmail.com>
2021-12-21 16:07:51 +0530

    edit again

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const connection =
-    "mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb?retryWrites=true&w=majority";
+    "mongodb://localhost:27017/meandb";
+// const connection =
+//     "mongodb+srv://meanapp:meanapp@123@cluster0.xqas2.mongodb.net/meandb?retryWrites=true&w=majority";
 mongoose
     .connect(connection)
     .then(() => console.log("Database Connected Successfully"))
